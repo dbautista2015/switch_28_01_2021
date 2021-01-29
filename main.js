fefbe99a79fd83c5f5f38f0f1a3f180448c39476
Diana Patricia Bautista Otalora <diana.bautista.otalora@gmail.com>
2021-01-28 19:03:24 -0500

    switch

---
+++ main.js
@@ -0,0 +1,15 @@
+//estructura del switch
+let key = prompt("Ingrese una opción: ");
+switch (key) {
+    case "1":
+        console.log(`Opción uno`);
+        break;
+    case "2":
+        console.log(`Opción dos`);
+        break;
+         
+    default:
+        console.log(`Opción incorrecta`);
+        break;
+}
+
